e14e944e414f0f0b2cab8e7ffaed2c42f14653a9
Mina Mikhail <mina@fightingtheboss.com>
2017-08-09 10:08:14 -0400

    Add in-class code for 06 Programmng Fundamentals Pt 1

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -5,8 +5,27 @@
 # (optionally) otherwise
 #   then do something else
 
+count = 12
 
+if count > 10
+  puts "Count is greater than ten"
+elsif count < 10
+  puts "Count is less than ten"
+else
+  puts "Count is equal to ten"
+end
 
+unless count > 10
+  puts "Count is less than or equal to ten"
+end
+
+puts "Count is less than or equal to ten" unless count > 10
+puts "Count is less than or equal to ten" if count <= 10
+
+cars = 50 if count > 10
+
+life_meter = (count > 10) ? count * 200 : count
+puts life_meter
 
 
 # Looping
