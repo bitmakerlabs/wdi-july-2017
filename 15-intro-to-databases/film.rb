428bf4f15179e8dffa86bdc82459e44958101725
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-08-21 13:17:33 -0400

    In class code

--- 15-intro-to-databases/film.rb
+++ 15-intro-to-databases/film.rb
@@ -1,70 +1,14 @@
-class Film
-  @@library = []
-  @@id = 1;
+gem "activerecord", "=4.2.9"
+require "active_record"
+require "mini_record"
 
-  def initialize(options)
-    @title = options[:title]
-    @country = options[:country]
-    @genre = options[:genre]
-    @rating = options[:rating]
-    @description = options[:description]
-    @id = @@id
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'films-ar.sqlite3')
 
-    @@id += 1
-  end
-
-  def id
-    @id
-  end
-
-  def title
-    @title
-  end
-
-  def title=(new_title)
-    @title = new_title
-  end
-
-  def country
-    @country
-  end
-
-  def country=(new_country)
-    @country = new_country
-  end
-
-  def genre
-    @genre
-  end
-
-  def genre=(new_genre)
-    @genre = new_genre
-  end
-
-  def rating
-    @rating
-  end
-
-  def rating=(new_rating)
-    @rating = new_rating
-  end
-
-  def description
-    @description
-  end
-
-  def description=(new_description)
-    @description = new_description
-  end
-
-  def self.create(options)
-    film = self.new(options)
-    @@library << film
-    return film
-  end
-
-  def self.all
-    return @@library
-  end
-
+class Film < ActiveRecord::Base
+  field :title, as: :string
+  field :country, as: :string
+  field :genre, as: :string
+  field :rating, as: :integer
+  field :description, as: :string
 end
+Film.auto_upgrade!
