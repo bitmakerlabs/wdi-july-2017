2c2314c65b36458c32f648a7e3392226cf27acf6
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-08-30 12:26:46 -0400

    [22-routing-and-controllers] Convert to resourceful routes

--- 22-routing-and-controllers/instructor/crm/config/routes.rb
+++ 22-routing-and-controllers/instructor/crm/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get    '/contacts'          => 'contacts#index'
-  get    '/contacts/new'      => 'contacts#new', as: 'new_contact'
-  get    '/contacts/:id'      => 'contacts#show', as: 'contact'
-  post   '/contacts'          => 'contacts#create'
-  get    '/contacts/:id/edit' => 'contacts#edit', as: 'edit_contact'
-  patch  '/contacts/:id'      => 'contacts#update'
-  delete '/contacts/:id'      => 'contacts#destroy'
+  # get    'contacts'          => 'contacts#index'                     # (R)ead
+  # get    'contacts/new'      => 'contacts#new', as: 'new_contact'    # (C)reate
+  # get    'contacts/:id'      => 'contacts#show', as: 'contact'       # (R)ead
+  # post   'contacts'          => 'contacts#create'                    # (C)reate
+  # get    'contacts/:id/edit' => 'contacts#edit', as: 'edit_contact'  # (U)pdate
+  # patch  'contacts/:id'      => 'contacts#update'                    # (U)pdate
+  # delete 'contacts/:id'      => 'contacts#destroy'                   # (D)elete
+  resources(:contacts)
 
   root 'contacts#index'
 end
