978f08fc847d01f847846c5d60cb4acfd755bd5c
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-08-30 11:12:57 -0400

    [22-routing-and-controllers] Remove render lines in the controller

--- 22-routing-and-controllers/instructor/crm/app/controllers/contacts_controller.rb
+++ 22-routing-and-controllers/instructor/crm/app/controllers/contacts_controller.rb
@@ -1,14 +1,10 @@
 class ContactsController < ApplicationController
 
-  # Part 2: Do something if route is hit
   def index
-    render :index
   end
 
   def show
     @contact = params[:id]
-
-    render :show
   end
 
 end
