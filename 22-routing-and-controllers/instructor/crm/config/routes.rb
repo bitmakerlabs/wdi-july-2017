019886052eaa0c7e81283508c002a380e8f902fb
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-08-30 11:40:27 -0400

    [22-routing-and-controllers] Use Named Helper

--- 22-routing-and-controllers/instructor/crm/config/routes.rb
+++ 22-routing-and-controllers/instructor/crm/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/contacts'     => 'contacts#index'
-  get '/contacts/:id' => 'contacts#show'
+  get '/contacts/:id' => 'contacts#show', as: 'contact'
 end
