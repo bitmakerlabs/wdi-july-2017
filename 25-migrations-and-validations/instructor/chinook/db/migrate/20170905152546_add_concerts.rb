82f1f7308ae48edb88033cb05ca561296c1defcf
Fred Ngo <fredngo@gmail.com>
2017-09-05 11:28:50 -0400

    [25-migrations-and-validations] Add Concert model and table

---
+++ 25-migrations-and-validations/instructor/chinook/db/migrate/20170905152546_add_concerts.rb
@@ -0,0 +1,14 @@
+class AddConcerts < ActiveRecord::Migration[5.1]
+  def change
+
+    create_table :concerts do |t|
+      t.string :name
+      t.datetime :played_at
+      t.integer :attendance
+      t.text :description
+
+      t.timestamps
+    end
+
+  end
+end
