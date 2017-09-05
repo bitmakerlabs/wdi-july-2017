adb686faa3dea56b0d153278d37a87fda2e418d1
Fred Ngo <fredngo@gmail.com>
2017-09-05 10:52:15 -0400

    [25-migrations-and-validations] Convert class methods to scopes

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -3,28 +3,21 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
-  def self.starts_with(char)
-    where('name ILIKE ?', "#{ char }%")
-  end
+  scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%") }
+  scope :short, -> { shorter_than(SHORT) }
+  scope :long, -> { longer_than_or_equal_to(LONG) }
+  scope :medium, -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
+  scope :shorter_than, -> (milliseconds) { where('milliseconds < ?', milliseconds) }
+  scope :longer_than_or_equal_to, -> (milliseconds) { where('milliseconds >= ?', milliseconds) }
 
-  def self.short
-    shorter_than(SHORT)
-  end
+  scope :most_recent_scope, -> (limit) { order(created_at: :desc).limit(limit) }
 
-  def self.long
-    longer_than_or_equal_to(LONG)
-  end
-
-  def self.medium
-    longer_than_or_equal_to(SHORT).shorter_than(LONG)
-  end
-
-  def self.shorter_than(milliseconds)
-    where('milliseconds < ?', milliseconds)
-  end
-
-  def self.longer_than_or_equal_to(milliseconds)
-    where('milliseconds >= ?', milliseconds)
+  def self.most_recent_method(limit)
+    if limit.present?
+      order(created_at: :desc).limit(limit)
+    else
+      none
+    end
   end
 
 end
