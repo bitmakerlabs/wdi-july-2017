eaf215762cd39584aee36910f2ee1f2ac07e9b1f
NatalieBlack <natalievictoriablack@gmail.com>
2017-09-06 11:58:14 -0400

    in-class code for 2nd half of associations lesson

--- 26-associations/photogur/db/schema.rb
+++ 26-associations/photogur/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906143713) do
+ActiveRecord::Schema.define(version: 20170906152652) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "picture_id"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 20170906143713) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "pictures_tags", force: :cascade do |t|
+    t.integer "picture_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
