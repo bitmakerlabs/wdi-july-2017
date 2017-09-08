271cf3b2c4eeff45aa8b8413484a4e5d5241708e
Fred Ngo <fredngo@gmail.com>
2017-09-08 11:29:02 -0400

    [28-sessions-and-authentication] Implement Users#create

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/users_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new
+
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 end
