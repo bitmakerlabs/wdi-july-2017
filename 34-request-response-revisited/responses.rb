962f338c0a682f54073fe6773a8b9e449b092328
NatalieBlack <natalievictoriablack@gmail.com>
2017-09-22 10:54:38 -0400

    in-class code for first half of request-response-revisited

--- 34-request-response-revisited/responses.rb
+++ 34-request-response-revisited/responses.rb
@@ -1,19 +1,8 @@
 require 'httparty'
+html_response = HTTParty.get('http://localhost:3000/monsters')
+puts html_response.body
+text_response = HTTParty.get('http://localhost:3000/monsters.text')
+json_response = HTTParty.get('http://localhost:3000/monsters.json')
 
-# Standard library that parses string data into JSON format
-require 'json'
-
-# Standard library that parses string data into CSV format
-require 'csv'
-
-# Let's make some HTTP requests to a server and see what we get back
-# https://github.com/bitmakerlabs/response-types-demo
-# HTTParty.get('example.com/index.html')
-
-#make get request to localhost:3000/monsters
-#result = HTTParty.get('http://localhost:3000/monsters')
-#puts result
-
-#result = HTTParty.get('http://localhost:3000/monsters.text')
-#puts result
-
+ttc_response = HTTParty.get("http://myttc.ca/finch_station.json")
+ttc_json = JSON.parse(ttc_response.body)
