7c66c9bfea537faf2554186846d8e584e5d0e985
Mina Mikhail <mina@fightingtheboss.com>
2017-10-02 12:44:30 -0400

    Add in-class code for 39 JS Events

--- 39-events/delegate.js
+++ 39-events/delegate.js
@@ -1,25 +1,41 @@
 // Global scope
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function() {
   // Function scope
 
   var count = 0,
       colors = ['tomato', 'azure', 'green', 'orange', 'purple', 'chartreuse', 'yellow', 'chocolate'];
 
+  var container = document.querySelector('#container');
 
-  $('#box-maker').on('click', function() {
+  container.addEventListener('click', function(e) {
+    if ( e.target.className === 'circle' ) {
+      e.target.remove();
+    }
+  });
+
+  var boxMaker = document.querySelector('#box-maker');
+
+  boxMaker.addEventListener('click', function() {
       var x = Math.ceil(Math.random() * 400),
           y = Math.ceil(Math.random() * 200),
-          c = Math.ceil(Math.random() * 8);
+          c = Math.ceil(Math.random() * 7);
 
-      $('<div class="circle"></div>')
-          .css({
-              top: y,
-              left: x,
-              backgroundColor: colors[c]
-          })
-          .html(count++)
-          .appendTo('#container');
+
+      var circle = document.createElement('div');
+
+      circle.className = 'circle';
+      circle.style.top = y + 'px';
+      circle.style.left = x + 'px';
+      circle.style.backgroundColor = colors[c];
+      circle.innerHTML = count++;
+
+      // circle.addEventListener('click', function() {
+      //   this.remove();
+      //   console.log('Removing a circle');
+      // });
+
+      container.append(circle);
 
       console.log('link was clicked');
   });
--- 39-events/script.js
+++ 39-events/script.js
@@ -1,11 +1,28 @@
-$(document).on('ready', function() {
+document.addEventListener('DOMContentLoaded', function() {
+
+  var clickBait = document.querySelector('#click-bait');
+
+  clickBait.addEventListener('click', function(eventObject) {
+    eventObject.preventDefault();
+    eventObject.stopPropagation();
+    window.alert('#click-bait was clicked!');
+  });
+
+  var secondLevel = document.querySelector('#second-level');
+
+  function secondLevelEventHandler(e) {
+    e.stopPropagation();
+    window.alert('#second-level received a click!');
+  }
+
+  secondLevel.addEventListener('click', secondLevelEventHandler);
+
+  var firstLevel = document.querySelector('#first-level');
+
+  var firstLevelEventHandler = function() {
+    window.alert('#first-level received a click!');
+  }
+
+  firstLevel.addEventListener('click', firstLevelEventHandler);
 
 });
-
-// $(function() {
-//
-// });
-
-// $(document).ready(function() {
-//
-// });
